436b40d45453b3db3dce03fab9606a36ecc098a1
Minxuan Liu <realnimiliu@gmail.com>
2021-03-02 04:17:20 -0800

    budget screen popup window triggered by clicking edit icon

--- public/js/budget_action.js
+++ public/js/budget_action.js
@@ -10,24 +10,49 @@ $(document).ready(function() {
  */
 function initializePage() {
     // initBudgetForm();
-    $(".each-budget img").click(function() {
-        var prev_div = $(this).prev();
-        var old_budget = $(prev_div).text();
-        var budget_val = old_budget == "Unset" ? 10 : parseFloat(old_budget.substring(1));
-        console.log("new budget: " + budget_val);
-        var cate_name = $(prev_div).attr('id').substring(6);
-        console.log("cate_name: " + cate_name);
-        $(prev_div).html('');
-        $('<input></input>').attr(
-            {
-            'type': 'number',
-            'step': "any",
-            'name': 'input' + cate_name,
-            'id': 'input' + cate_name,
-            'placeholder': "$10 ~ $40k",
-            "min": 10,
-            "max": 40000,
-            }).appendTo(prev_div);
+    // $(function() {
+    //     function setBudget() {
+    //         alert("yes!");
+    //     }
+
+    //     dialog = $( "#budget-dialog" ).dialog({
+    //         autoOpen: false,
+    //         height: 300,
+    //         width: 200,
+    //         modal: true,
+    //         buttons: {
+    //           "Set": setBudget,
+    //           Cancel: function() {
+    //             dialog.dialog( "close" );
+    //           }
+    //         }
+    //     });
+
+    //     $(".each-budget img").click(function() {
+    //         dialog.dialog( "open" );
+    //         // var prev_div = $(this).prev();
+    //         // var old_budget = $(prev_div).text();
+    //         // var budget_val = old_budget == "Unset" ? 10 : parseFloat(old_budget.substring(1));
+    //         // console.log("new budget: " + budget_val);
+    //         // var cate_name = $(prev_div).attr('id').substring(6);
+    //         // console.log("cate_name: " + cate_name);
+    //         // $(prev_div).html('');
+    //         // $('<input></input>').attr(
+    //         //     {
+    //         //     'type': 'number',
+    //         //     'step': "any",
+    //         //     'name': 'input' + cate_name,
+    //         //     'id': 'input' + cate_name,
+    //         //     'placeholder': "$10 ~ $40k",
+    //         //     "min": 10,
+    //         //     "max": 40000,
+    //         //     }).appendTo(prev_div);
+    //         // $("#input"+cate_name).focus().val(budget_val);
+    //     });
+    // });
+
+}
+
         // $("#input"+cate_name).css({
         //     "padding-left": "20px"
         // });
@@ -36,13 +61,8 @@ function initializePage() {
         //     "fontSize": "20px",
         //     "position": "absolute"
         // });
-        $("#input"+cate_name).focus().val(budget_val);
-    });
-}
 
 function initBudgetForm() {
-    // $("#newEntryForm").submit(
 
-    // );
 }
 

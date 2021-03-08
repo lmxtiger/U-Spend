1e8974dc8187678eb77db1e72342069797a3ee2d
Minxuan Liu <realnimiliu@gmail.com>
2021-03-07 20:01:46 -0800

    overHist ? icon ==> explain what the progress bar means

--- public/js/overHist_action.js
+++ public/js/overHist_action.js
@@ -4,7 +4,29 @@
 $(document).ready(function() {
     toggle_dropdown();
     adjustCateProgBar();
-})
+    displayQMDialog();
+});
+
+function displayQMDialog() {
+    $(function() {
+        var dialog = $("div#dialog").dialog({
+            autoOpen: false,
+            modal: true,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function() {
+                        dialog.dialog( "close" );
+                    }
+                }
+            ]
+        });
+
+        $("img#question-mark").click(function() {
+            dialog.dialog("open");
+        });
+    });
+};
 
 function toggle_dropdown() {
     // $( "#dropbtn" ).click(function() {
@@ -37,11 +59,13 @@ function postGet(data) {
             // console.log(cateName + " inPercent: " + inPercent);
             $(this).width(inPercent + "%");
             if(inPercent === 100) {
-                $(this).css(
-                    {"background-color": "red",
-                    "border-top-right-radius": "8px",
-                    "border-bottom-right-radius": "8px"}
-                );
+                $(this).width("0px");
+                $(this).parent().css("background-color", "red");
+                // $(this).css(
+                //     {"background-color": "red",
+                //     "border-top-right-radius": "8px",
+                //     "border-bottom-right-radius": "8px"}
+                // );
             }
         }else{
             $(this).width("0px");

a56c794cf5609073ba99a8d1f6b74c56be757199
lmxtiger <44688205+lmxtiger@users.noreply.github.com>
2021-03-13 11:55:17 -0800

    Update signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -9,18 +9,18 @@ exports.view = function(req, res){
 	res.render('signup');
 };
 
-// exports.accounts = function(req, res) {
-// 	res.json(accounts.accounts);
-// };
+exports.accounts = function(req, res) {
+	res.json(accounts.accounts);
+};
 
-// exports.newAccount = function(req, res) {
-// 	accounts.accounts[req.body.username] = {"password": req.body.password, "petName": req.body.petName};
+exports.newAccount = function(req, res) {
+	accounts.accounts[req.body.username] = {"password": req.body.password, "petName": req.body.petName};
 
-// 	home_data.petName = req.body.petName;
-// 	home_data.fb_whole_name = req.body.username;
-// 	home_data.fb_profile_url = "/css/pics/accountPic.png";
+	home_data.petName = req.body.petName;
+	home_data.fb_whole_name = req.body.username;
+	home_data.fb_profile_url = "/css/pics/accountPic.png";
 	
-// 	console.log("signup newAccount");
-// 	console.log(accounts.accounts);
-// 	res.send(accounts.accounts); // required in order for signup page to redirect user to home page
-// };
+	console.log("signup newAccount");
+	console.log(accounts.accounts);
+	res.send(accounts.accounts); // required in order for signup page to redirect user to home page
+};
